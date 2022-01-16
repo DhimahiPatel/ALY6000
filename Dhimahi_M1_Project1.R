f26c851fda82a9a7006fbbe2a4f72ae4ca82ef47
Jimi <48714091+DhimahiPatel@users.noreply.github.com>
2022-01-16 13:45:12 -0500

    Add files via upload

---
+++ Dhimahi_M1_Project1.R
@@ -0,0 +1,31 @@
+print("Dhimahi Patel", quote = FALSE)
+
+install.packages("vcd")
+
+library(vcd)
+
+sales <- c(7, 11, 15, 20, 19, 11, 18, 10, 6, 22)
+temperature <-c(69, 81, 77, 84, 80, 97, 87, 70, 65, 90)
+mean(temperature)
+sd(temperature)
+cor(sales, temperature)
+plot(sales, temperature)
+
+sales <- sales[! sales %in% c(20)]
+sales
+sales <- append(sales, 16, 3)
+sales
+
+icSales <- data.frame(sales, temperature)
+str(icSales)
+summary(icSales)
+
+x <- c("Tom", "Dick", "Harry")
+x
+y <- matrix(1:10, nrow = 5, ncol = 2)
+y
+
+student <- read.csv(file= 'C://Users//DELL//Downloads//Student.csv', header=TRUE, sep=",")
+names(student)
+
+q()
